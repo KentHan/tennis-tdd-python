d9afb250d5469d2db5130dcf64e0a271f5a47eac
Kent Han <--replace-all>
2019-07-05 00:32:07 +0800

    Finish before deuce and win

--- TennisScore.py
+++ TennisScore.py
@@ -1,15 +1,20 @@
 class TennisScore(object):
 
     def __init__(self):
+        self.score_map = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
+        self.second_player_score = 0
         self.first_player_score = 0
 
     def first_player_scored(self):
-        self.first_player_score +=1
+        self.first_player_score += 1
+
+    def second_player_scored(self):
+        self.second_player_score += 1
 
     def score(self):
-        if self.first_player_score is 1:
-            return "Fifteen Love"
-        if self.first_player_score is 2:
-            return "Thirty Love"
-        return "Love All"
-
+        if self.first_player_score is self.second_player_score:
+            return f"{self.score_map[self.first_player_score]} All"
+        if self.second_player_score > 0:
+            return f"Love {self.score_map[self.second_player_score]}"
+        if self.first_player_score > 0:
+            return f"{self.score_map[self.first_player_score]} Love"
--- TennisScoreTest.py
+++ TennisScoreTest.py
@@ -19,9 +19,26 @@ class TennisScoreTest(unittest.TestCase):
         self.first_player_scored(2)
         self.assertEqual("Thirty Love", self.sut.score())
 
+    def test_forty_love(self):
+        self.first_player_scored(3)
+        self.assertEqual("Forty Love", self.sut.score())
+
+    def test_love_fifteen(self):
+        self.sut.second_player_scored()
+        self.assertEqual("Love Fifteen", self.sut.score())
+
+    def test_fifteen_all(self):
+        self.sut.first_player_scored()
+        self.sut.second_player_scored()
+        self.assertEqual("Fifteen All", self.sut.score())
+
     def first_player_scored(self, times):
         for i in range(times):
             self.sut.first_player_scored()
+
+    def second_player_scored(self, times):
+        for i in range(times):
+            self.sut.second_player_scored()
 
 
 if __name__ == '__main__':
