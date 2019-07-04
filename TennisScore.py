a159b8efe0c229381e74f0010935401530bcac57
Kent Han <--replace-all>
2019-07-05 00:11:43 +0800

    Complete First player scores 1 or 2

--- TennisScore.py
+++ TennisScore.py
@@ -1,3 +1,15 @@
 class TennisScore(object):
+
+    def __init__(self):
+        self.first_player_score = 0
+
+    def first_player_scored(self):
+        self.first_player_score +=1
+
     def score(self):
+        if self.first_player_score is 1:
+            return "Fifteen Love"
+        if self.first_player_score is 2:
+            return "Thirty Love"
         return "Love All"
+
--- TennisScoreTest.py
+++ TennisScoreTest.py
@@ -5,9 +5,24 @@ from TennisScore import TennisScore
 
 class TennisScoreTest(unittest.TestCase):
 
+    def setUp(self):
+        self.sut = TennisScore()
+
     def test_love_all(self):
-        sut = TennisScore()
-        self.assertEqual("Love All", sut.score())
+        self.assertEqual("Love All", self.sut.score())
+
+    def test_fifteen_love(self):
+        self.first_player_scored(1)
+        self.assertEqual("Fifteen Love", self.sut.score())
+
+    def test_thirty_love(self):
+        self.first_player_scored(2)
+        self.assertEqual("Thirty Love", self.sut.score())
+
+    def first_player_scored(self, times):
+        for i in range(times):
+            self.sut.first_player_scored()
+
 
 if __name__ == '__main__':
     unittest.main()
