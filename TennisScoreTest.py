28a1beb24037808403b9bcbbb4a4954ef72fd2d9
Kent Han <--replace-all>
2019-07-07 23:55:27 +0800

    Refactor the assertion by extracting method

--- TennisScoreTest.py
+++ TennisScoreTest.py
@@ -9,47 +9,50 @@ class TennisScoreTest(unittest.TestCase):
         self.sut = TennisScore(f"Ace", f"Pro")
 
     def test_love_all(self):
-        self.assertEqual("Love All", self.sut.score())
+        self.score_should_be("Love All")
+
+    def score_should_be(self, score):
+        self.assertEqual(score, self.sut.score())
 
     def test_fifteen_love(self):
         self.first_player_scored(1)
-        self.assertEqual("Fifteen Love", self.sut.score())
+        self.score_should_be("Fifteen Love")
 
     def test_thirty_love(self):
         self.first_player_scored(2)
-        self.assertEqual("Thirty Love", self.sut.score())
+        self.score_should_be("Thirty Love")
 
     def test_forty_love(self):
         self.first_player_scored(3)
-        self.assertEqual("Forty Love", self.sut.score())
+        self.score_should_be("Forty Love")
 
     def test_love_fifteen(self):
         self.second_player_scored(1)
-        self.assertEqual("Love Fifteen", self.sut.score())
+        self.score_should_be("Love Fifteen")
 
     def test_fifteen_all(self):
         self.first_player_scored(1)
         self.second_player_scored(1)
-        self.assertEqual("Fifteen All", self.sut.score())
+        self.score_should_be("Fifteen All")
 
     def test_deuce(self):
         self.deuce()
-        self.assertEqual("Deuce", self.sut.score())
+        self.score_should_be("Deuce")
 
     def test_first_player_adv(self):
         self.deuce()
         self.first_player_scored(1)
-        self.assertEqual("Ace adv", self.sut.score())
+        self.score_should_be("Ace adv")
 
     def test_first_player_win(self):
         self.deuce()
         self.first_player_scored(2)
-        self.assertEqual("Ace win", self.sut.score())
+        self.score_should_be("Ace win")
 
     def test_second_player_win(self):
         self.first_player_scored(2)
         self.second_player_scored(4)
-        self.assertEqual("Pro win", self.sut.score())
+        self.score_should_be("Pro win")
 
     def deuce(self):
         self.first_player_scored(3)
