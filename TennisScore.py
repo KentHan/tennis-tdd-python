723f85e2f6ce0b38b7f81e9b69e0b3b95669382a
Kent Han <--replace-all>
2019-07-07 23:51:42 +0800

    Finish implementation by passing all tests

--- TennisScore.py
+++ TennisScore.py
@@ -1,9 +1,11 @@
 class TennisScore(object):
 
-    def __init__(self):
+    def __init__(self, first_player_name, second_player_name):
         self.score_map = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
         self.second_player_score = 0
         self.first_player_score = 0
+        self.first_player_name = first_player_name
+        self.second_player_name = second_player_name
 
     def first_player_scored(self):
         self.first_player_score += 1
@@ -12,9 +14,32 @@ class TennisScore(object):
         self.second_player_score += 1
 
     def score(self):
-        if self.first_player_score is self.second_player_score:
-            return f"{self.score_map[self.first_player_score]} All"
-        if self.second_player_score > 0:
-            return f"Love {self.score_map[self.second_player_score]}"
-        if self.first_player_score > 0:
-            return f"{self.score_map[self.first_player_score]} Love"
+        return (self.deuce() if self.is_deuce() else self.same_score()) if self.is_same_score() else (
+            self.adv_score() if self.is_adv_state() else self.look_up_score())
+
+    def look_up_score(self):
+        return f"{self.score_map[self.first_player_score]} {self.score_map[self.second_player_score]}"
+
+    def adv_score(self):
+        return f"{self.winner()} adv" if self.game_point() else f"{self.winner()} win"
+
+    def same_score(self):
+        return f"{self.score_map[self.first_player_score]} All"
+
+    def deuce(self):
+        return f"Deuce"
+
+    def game_point(self):
+        return abs(self.first_player_score - self.second_player_score) == 1
+
+    def winner(self):
+        return self.first_player_name if self.first_player_score > self.second_player_score else self.second_player_name
+
+    def is_same_score(self):
+        return self.first_player_score is self.second_player_score
+
+    def is_deuce(self):
+        return self.first_player_score >= 3
+
+    def is_adv_state(self):
+        return self.first_player_score > 3 or self.second_player_score > 3
--- TennisScoreTest.py
+++ TennisScoreTest.py
@@ -6,7 +6,7 @@ from TennisScore import TennisScore
 class TennisScoreTest(unittest.TestCase):
 
     def setUp(self):
-        self.sut = TennisScore()
+        self.sut = TennisScore(f"Ace", f"Pro")
 
     def test_love_all(self):
         self.assertEqual("Love All", self.sut.score())
@@ -24,13 +24,36 @@ class TennisScoreTest(unittest.TestCase):
         self.assertEqual("Forty Love", self.sut.score())
 
     def test_love_fifteen(self):
-        self.sut.second_player_scored()
+        self.second_player_scored(1)
         self.assertEqual("Love Fifteen", self.sut.score())
 
     def test_fifteen_all(self):
-        self.sut.first_player_scored()
-        self.sut.second_player_scored()
+        self.first_player_scored(1)
+        self.second_player_scored(1)
         self.assertEqual("Fifteen All", self.sut.score())
+
+    def test_deuce(self):
+        self.deuce()
+        self.assertEqual("Deuce", self.sut.score())
+
+    def test_first_player_adv(self):
+        self.deuce()
+        self.first_player_scored(1)
+        self.assertEqual("Ace adv", self.sut.score())
+
+    def test_first_player_win(self):
+        self.deuce()
+        self.first_player_scored(2)
+        self.assertEqual("Ace win", self.sut.score())
+
+    def test_second_player_win(self):
+        self.first_player_scored(2)
+        self.second_player_scored(4)
+        self.assertEqual("Pro win", self.sut.score())
+
+    def deuce(self):
+        self.first_player_scored(3)
+        self.second_player_scored(3)
 
     def first_player_scored(self, times):
         for i in range(times):
