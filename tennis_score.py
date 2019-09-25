fbbf3cef201106094e719c1a077bc2e27c51498e
Kent Han <kent.han@linecorp.com>
2019-09-25 16:06:45 +0800

    add Fifteen Love

--- tennis_score.py
+++ tennis_score.py
@@ -1,3 +1,18 @@
 class TennisScore(object):
-    def getScore(self):
-        return "Love All"
+    def __init__(self):
+        self.player_1_score = 0
+        self.player_2_score = 0
+
+    def get_score(self):
+        if self.player_1_score + self.player_2_score == 0:
+            return "Love All"
+        elif self.player_1_score == 1 and self.player_2_score == 0:
+            return "Fifteen Love"
+
+    def add_score(self, player):
+        if player == 1:
+            self.player_1_score += 1
+        elif player == 2:
+            self.player_2_score += 1
+        else:
+            raise Exception("only two player")
--- tennis_score_test.py
+++ tennis_score_test.py
@@ -7,4 +7,10 @@ class TennisScoreTest(unittest.TestCase):
 
    def test_love_all(self):
        sut = TennisScore()
-       self.assertEqual("Love All", sut.getScore())
+       self.assertEqual("Love All", sut.get_score())
+
+   def test_fiften_love(self):
+       sut = TennisScore()
+       sut.add_score(1)
+       self.assertEqual("Fifteen Love", sut.get_score())
+
