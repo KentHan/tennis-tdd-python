d5c8ddf5b3be47db0645802b69cd4d424dc5e5b3
Kent Han <kent.han@linecorp.com>
2019-09-25 16:41:02 +0800

    refactor: add setUp

--- tennis_score_test.py
+++ tennis_score_test.py
@@ -4,43 +4,38 @@ from tennis_score import TennisScore
 
 
 class TennisScoreTest(unittest.TestCase):
+   def setUp(self):
+       self.sut = TennisScore()
+    
 
    def test_love_all(self):
-       sut = TennisScore()
-       self.assertEqual("Love All", sut.get_score())
+       self.assertEqual("Love All", self.sut.get_score())
 
    def test_fifteen_love(self):
-       sut = TennisScore()
-       sut.add_score(1,1)
-       self.assertEqual("Fifteen Love", sut.get_score())
+       self.sut.add_score(1,1)
+       self.assertEqual("Fifteen Love", self.sut.get_score())
 
    def test_thirty_love(self):
-       sut = TennisScore()
-       sut.add_score(1,2)
-       self.assertEqual("Thirty Love", sut.get_score())
+       self.sut.add_score(1,2)
+       self.assertEqual("Thirty Love", self.sut.get_score())
 
    def test_forty_love(self):
-       sut = TennisScore()
-       sut.add_score(1, 3)
-       self.assertEqual("Forty Love", sut.get_score())
+       self.sut.add_score(1, 3)
+       self.assertEqual("Forty Love", self.sut.get_score())
 
    def test_love_fifteen(self):
-       sut = TennisScore()
-       sut.add_score(2, 1)
-       self.assertEqual("Love Fifteen", sut.get_score())
+       self.sut.add_score(2, 1)
+       self.assertEqual("Love Fifteen", self.sut.get_score())
 
    def test_love_thirty(self):
-       sut = TennisScore()
-       sut.add_score(2, 2)
-       self.assertEqual("Love Thirty", sut.get_score())
+       self.sut.add_score(2, 2)
+       self.assertEqual("Love Thirty", self.sut.get_score())
 
    def test_love_forty(self):
-       sut = TennisScore()
-       sut.add_score(2, 3)
-       self.assertEqual("Love Forty", sut.get_score())
+       self.sut.add_score(2, 3)
+       self.assertEqual("Love Forty", self.sut.get_score())
 
    def test_fifteen_all(self):
-       sut = TennisScore()
-       sut.add_score(1, 1)
-       sut.add_score(2, 1)
-       self.assertEqual("Fifteen All", sut.get_score())
+       self.sut.add_score(1, 1)
+       self.sut.add_score(2, 1)
+       self.assertEqual("Fifteen All", self.sut.get_score())
